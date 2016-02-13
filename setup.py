6d0a9584e3dd4fbd1b695e17dd126521b007c5c8
femnad <zehemer@fastmail.fm>
2016-02-13 16:58:56 +0200

    Fix Member Variable Access Error
    * Also switch to pbr

--- setup.py
+++ setup.py
@@ -2,10 +2,5 @@
 import setuptools
 
 setuptools.setup(
-    name='did_you_client',
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    entry_points={'console_scripts':
-                  ['did_you_client = did_you_client.command:run_client',
-                   'did_you_checker = did_you_client.command:run_checker']},
-    )
+    setup_requires=['pbr>=1.8'],
+    use_pbr=True)
